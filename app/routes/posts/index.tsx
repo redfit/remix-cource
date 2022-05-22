ebc0d5072a513d85b0150966ebadf4b3251dbdbf
daisuke fuchiwaki <fuchiwaki.d@note.jp>
2022-05-22 23:26:56 +0900

    Fetch Data in Remix Using a Basic Loader

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,7 +1,34 @@
+import { Link, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+
+export const loader = async () => {
+  const posts = [
+    {
+      slug: "my-first-post",
+      title: "My First Post!",
+    },
+    {
+      slug: "trail-riding-with-onewheel",
+      title: "Trail Riding with Onewheel",
+    },
+  ];
+  const postsString = JSON.stringify({ posts });
+  console.log("loader");
+  return json({ posts });
+};
+
 export default function PostsRoute() {
+  const { posts } = useLoaderData();
   return (
     <main>
       <h1>Posts</h1>
+      {posts.map((post) => (
+        <li key={post.slug} className="list-none">
+          <Link to={post.slug} className="text-blue-600 underline">
+            {post.title}
+          </Link>
+        </li>
+      ))}
     </main>
   );
 }
