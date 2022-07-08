c3e698a7dc4d49683469d8913cdb964adca6030d
daisuke fuchiwaki <fuchiwaki.d@note.jp>
2022-07-08 21:53:46 +0900

    Typing Form Submission Values with tiny-invariant

--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -2,6 +2,15 @@ import { Form, useActionData } from "@remix-run/react";
 import { redirect } from "@remix-run/node";
 import { ActionFunction, json } from "@remix-run/node";
 import { createPost } from "~/models/post.server";
+import invariant from "tiny-invariant";
+
+type ActionData =
+  | {
+      title: null | string;
+      slug: null | string;
+      markdown: null | string;
+    }
+  | undefined;
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
@@ -10,7 +19,7 @@ export const action: ActionFunction = async ({ request }) => {
   const slug = formData.get("slug");
   const markdown = formData.get("markdown");
 
-  const errors = {
+  const errors: ActionData = {
     title: title ? null : "Title is required",
     slug: slug ? null : "Slug is required",
     markdown: markdown ? null : "Markdown is required",
@@ -18,9 +27,12 @@ export const action: ActionFunction = async ({ request }) => {
 
   const hasErrors = Object.values(errors).some((errorMessage) => errorMessage);
   if (hasErrors) {
-    return json(errors);
+    return json<ActionData>(errors);
   }
 
+  invariant(typeof title === "string", "title must be a string");
+  invariant(typeof slug === "string", "slug must be a string");
+  invariant(typeof markdown === "string", "markdown must be a string");
   await createPost({ title, slug, markdown });
   return redirect("/posts/admin");
 };
@@ -28,7 +40,7 @@ export const action: ActionFunction = async ({ request }) => {
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
 
 export default function NewPostRoute() {
-  const errors = useActionData();
+  const errors = useActionData() as ActionData;
   return (
     <Form method="post">
       <p>
